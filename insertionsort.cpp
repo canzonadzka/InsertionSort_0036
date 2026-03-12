79f4ab6dce0ae7e38c22ba5941dd9cad338f620a
canzonadzka <canzonadzka@gmail.com>
2026-03-12 14:50:54 +0800

    Menambahkan input element array

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -20,6 +20,17 @@ void input()
             cout << "\nArray Tidak Boleh Lebih dari 20\n";
         }
     }
+
+    cout << endl;
+    cout << "===============================";
+    cout << "\nMasukan Element Array";
+    cout << "\n===============================";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "\nData ke-" << (i + 1) << " : ";
+        cin >> arr[i];
+    }
 }
 
 int main()
