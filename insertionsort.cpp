3df89c7542af75453205e37d8a09b3dd0275fc6c
canzonadzka <canzonadzka@gmail.com>
2026-03-12 14:55:42 +0800

    Menambahkan fungsi display

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -59,8 +59,24 @@ void insertionsort()
     }
 }
 
+void display()
+{
+    cout << endl;
+    cout << "\n================";
+    cout << "\nElement Array";
+    cout << "\n================";
+
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl;
+    }
+
+    cout << endl;
+}
+
 int main()
 {
     input();
     insertionsort();
+    display();
 }
